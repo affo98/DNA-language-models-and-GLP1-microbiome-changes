d29432bfa46e634d259cbceda0f3bd435c0680b3
ah140797 <ah140797@gmail.com>
2025-03-05 12:55:17 +0100

    feature (KMEIOD) fix memory

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ def main():
 
     kmediod = KMediod(
         embeddings,
-        min_similarity=0.006,
+        min_similarity=0.007,
         min_bin_size=10,
         num_steps=3,
         max_iter=1000,
