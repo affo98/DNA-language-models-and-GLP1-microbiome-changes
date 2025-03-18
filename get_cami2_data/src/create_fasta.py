4384ca665d0ca04ab376810b6e9ccd950bf07ae9
ah140797 <ah140797@gmail.com>
2025-03-18 13:22:07 +0100

    ls

--- get_cami2_data/src/create_fasta.py
+++ get_cami2_data/src/create_fasta.py
@@ -21,60 +21,21 @@ if len(sys.argv) == 1:
 
 args = parser.parse_args()
 
-
-def byte_iterfasta(filehandle):
-    """Yields FastaEntries from a binary opened fasta file.
-
-    Usage:
-    >>> with Reader('/dir/fasta.fna') as filehandle:
-    ...     entries = byte_iterfasta(filehandle, '/dir/fasta/fna') # a generator
-
-    Inputs:
-        filehandle: Any iterator of binary lines of a FASTA file
-        comment: Ignore lines beginning with any whitespace + comment
-
-    Output: Generator of FastaEntry-objects from file
-    """
-
-    # Make it work for persistent iterators, e.g. lists
-    line_iterator = iter(filehandle)
-    # prefix = "" if filename is None else f"In file '{filename}', "
-    header = next(line_iterator, None)
-
-    # Empty file is valid - we return from the iterator
-    if header is None:
-        return None
-    elif not isinstance(header, bytes):
-        raise TypeError(
-            f"first line is not binary. Are you sure you are reading the file in binary mode?"
-        )
-
-    elif not header.startswith(b">"):
-        raise ValueError(f"FASTA file is invalid, first line does not begin with '>'")
-
-    elif isinstance(header, bytes):
-        print("yes")
-
-
-with vamb.vambtools.Reader(args.fastapath) as file:
-    byte_iterfasta(file)
-
-
 # Read in FASTA files only to get its length. This way, we can avoid storing
 # in memory contigs for sequences that will never get output anyway
-# lens: dict[str, int] = dict()
-# with vamb.vambtools.Reader(args.fastapath) as file:
-#     for record in vamb.vambtools.byte_iterfasta(file, args.fastapath):
-#         lens[record.identifier] = len(record)
+lens: dict[str, int] = dict()
+with vamb.vambtools.Reader(args.fastapath) as file:
+    for record in vamb.vambtools.byte_iterfasta(file, args.fastapath):
+        lens[record.identifier] = len(record)
 
-# with open(args.clusterspath) as file:
-#     clusters = vamb.vambtools.read_clusters(file)
+with open(args.clusterspath) as file:
+    clusters = vamb.vambtools.read_clusters(file)
 
-# clusters = {
-#     cluster: contigs
-#     for (cluster, contigs) in clusters.items()
-#     if sum(lens[c] for c in contigs) >= args.minsize
-# }
+clusters = {
+    cluster: contigs
+    for (cluster, contigs) in clusters.items()
+    if sum(lens[c] for c in contigs) >= args.minsize
+}
 
-# with vamb.vambtools.Reader(args.fastapath) as file:
-#     vamb.vambtools.write_bins(pathlib.Path(args.outdir), clusters, file, maxbins=None)
+with vamb.vambtools.Reader(args.fastapath) as file:
+    vamb.vambtools.write_bins(pathlib.Path(args.outdir), clusters, file, maxbins=None)
