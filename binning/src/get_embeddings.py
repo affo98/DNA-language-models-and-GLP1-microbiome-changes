cadf4269d77e214677e4a5b8416c944167dbdb7f
ah140797 <ah140797@gmail.com>
2025-03-27 10:36:29 +0100

    embeddings

--- binning/src/get_embeddings.py
+++ binning/src/get_embeddings.py
@@ -28,6 +28,8 @@ warnings.simplefilter("ignore", UserWarning)
 warnings.filterwarnings("ignore", message="Increasing alibi size")
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
+TOKENIZER_MAX_LENGTH = 2000
+
 
 def get_embeddings(
     dna_sequences: list[str],
@@ -152,6 +154,9 @@ def calculate_llm_embedding(
     sorted_dna_sequences, idx = sort_sequences(
         dna_sequences
     )  # To reduce Padding overhead
+
+    sorted_dna_sequences = [seq for seq in sorted_dna_sequences if len(seq) < 300000]
+
     dna_sequences = ContigDataset(sorted_dna_sequences)
 
     device, n_gpu = get_available_device()
@@ -163,7 +168,7 @@ def calculate_llm_embedding(
         trust_remote_code=True,
         padding="max_length",
     )
-    print(f"{model_name} tokenizer max length: {tokenizer.model_max_length}")
+    print(f"{model_name} tokenizer max length: {TOKENIZER_MAX_LENGTH}")
 
     config = BertConfig.from_pretrained(
         model_path,
@@ -192,7 +197,7 @@ def calculate_llm_embedding(
                 return_tensors="pt",
                 return_attention_mask=True,
                 padding=True,
-                max_length=2000,  # change to avoid OOM erros
+                max_length=TOKENIZER_MAX_LENGTH,  # change to avoid OOM erros
             )
 
             input_ids = inputs_tokenized["input_ids"].to(device)
