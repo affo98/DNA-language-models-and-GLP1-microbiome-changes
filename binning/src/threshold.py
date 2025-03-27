dec7ffff93fd8c6d7d3f8afec8ae274a5926f917
ah140797 <ah140797@gmail.com>
2025-03-27 17:37:47 +0100

    ds

--- binning/src/threshold.py
+++ binning/src/threshold.py
@@ -189,8 +189,8 @@ class Threshold:
         """
 
         for k in [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]:
-            self.knn_threshold, self.pairsim_vector, self.bin_vector = (
-                self.knn_threshold(k, p=0.7)
+            self.threshold, self.pairsim_vector, self.bin_vector = self.knn_threshold(
+                k, p=0.7
             )
 
             plt.figure(figsize=(8, 6))
@@ -231,7 +231,7 @@ class Threshold:
             #     label=f"Manual: {self.pairsim_vector[self.pairsim_vector[350:700].argmin()]:.5f}",
             # )
             plt.axvline(
-                self.knn_threshold, color="g", linestyle="--", label="KNN Threshold"
+                self.threshold, color="g", linestyle="--", label="KNN Threshold"
             )
 
             plt.plot(
