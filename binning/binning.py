1006fe84cd407666955db913c4370cd6c1cafa12
ah140797 <ah140797@gmail.com>
2025-03-28 15:58:27 +0100

    ds

--- binning/binning.py
+++ binning/binning.py
@@ -27,7 +27,7 @@ def main(args, log):
     contigs, contig_names = read_contigs(
         args.contigs, filter_len=MAX_CONTIG_LENGTH, log=log
     )
-    # contigs = contigs[0:10000]
+    contigs = contigs[0:2000]
 
     embedder = Embedder(
         contigs,
