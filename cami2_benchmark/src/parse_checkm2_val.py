ded2721eb4dfc8830f0fb9deb777d3e7c1441ca6
ah140797 <ah140797@gmail.com>
2025-04-02 10:38:39 +0200

    ds

--- cami2_benchmark/src/parse_checkm2_val.py
+++ cami2_benchmark/src/parse_checkm2_val.py
@@ -6,17 +6,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 COMPLETENESS_BINS = [90, 80, 70, 60, 50]
-CONTAMINATION_THRESHOLD = 5
+CONTAMINATION_THRESHOLD = 50
 
 
-def parse_quality_report(file_path):
+def parse_quality_report(file_path) -> pd.DataFrame:
     """Parses a CheckM2 quality report and extracts completeness & contamination."""
     df = pd.read_csv(file_path, sep="\t")
     df = df[df["Contamination"] < 5]
     return df["Completeness"].values
 
 
-def process_all_reports(results_dir):
+def process_all_reports(results_dir) -> dict:
     """Walks through the RESULTS_DIR to collect all results."""
     data = {}
 
@@ -31,6 +31,43 @@ def process_all_reports(results_dir):
         data[k_p_combination] = bin_counts
 
     return data
+
+
+# def plot_results(data)->None:
+#             plt.figure(figsize=(8, 6))
+
+#         if knn:
+#             plt.axvline(
+#                 self.knn_threshold,
+#                 color="g",
+#                 linestyle="--",
+#                 label=f"KNN Threshold: {self.knn_threshold} (k={self.knn_k}, p={self.knn_p})",
+#             )
+
+#         plt.plot(
+#             self.pairsim_vector,
+#             self.bin_vector,
+#             color="skyblue",
+#             linestyle="-",
+#             linewidth=2,
+#         )
+
+#         plt.xlabel("Similarity Bins")
+#         plt.ylabel("Frequency")
+#         plt.title(f"Similarity Histogram {self.model_name}")
+
+#         plt.legend()
+
+#         file_path = os.path.join(
+#             self.save_path,
+#             f"k{self.knn_k}_p{self.knn_p}_similarity_histogram.png",
+#         )
+#         plt.tight_layout()
+#         plt.savefig(file_path)
+#         plt.close()
+#         self.log.append(f"Plot saved at: {file_path}")
+
+#         return
 
 
 def main(args):
