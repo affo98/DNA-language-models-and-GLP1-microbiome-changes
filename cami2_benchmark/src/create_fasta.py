79111d61baa83fb6925f2bd7e3bf7daa64306d76
ah140797 <ah140797@gmail.com>
2025-04-02 10:12:08 +0200

    ds

--- cami2_benchmark/src/create_fasta.py
+++ cami2_benchmark/src/create_fasta.py
@@ -48,7 +48,7 @@ with vamb.vambtools.Reader(args.fastapath) as file:
 
 
 with open(args.log, "w") as log_f:
-    log_f.write(f"PATH: {args.clusterspath}")
+    log_f.write(f"PATH: {args.clusterspath}\n")
     log_f.write(f"Using minimum binsize of {args.minsize} base-pairs")
     log_f.write(f"Total clusters before filtering: {num_clusters_before}\n")
     log_f.write(f"Total clusters after filtering: {num_clusters_after}\n")
