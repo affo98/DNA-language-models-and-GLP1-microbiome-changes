b54c01aa544b9b9cb8275da3274169610818c84a
ah140797 <ah140797@gmail.com>
2025-04-12 13:16:58 +0200

    ds

--- binning/src/clustering_hubness.py
+++ binning/src/clustering_hubness.py
@@ -190,9 +190,7 @@ class KMediod:
 
                 sampled_indices = rng.choice(n_total, size=n_samples, replace=False)
                 sampled_embeddings = self.embeddings[sampled_indices]
-                sampled_embeddings = torch.concat(
-                    sampled_embeddings, seed.unsqueeze(0)
-                )  # add seed
+                sampled_embeddings = torch.concat(sampled_embeddings, seed)
 
                 # then compute similarities
                 similarities = torch.mm(sampled_embeddings, self.embeddings.T)
