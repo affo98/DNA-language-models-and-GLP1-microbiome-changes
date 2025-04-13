268b265426ceaa5669068e4201918ac91cfc8e91
ah140797 <ah140797@gmail.com>
2025-04-13 19:49:28 +0200

    ds

--- cami2_benchmark/sweeps/run_models.sh
+++ cami2_benchmark/sweeps/run_models.sh
@@ -1,29 +1,42 @@
 #!/bin/bash
 
-# Check if at least two arguments are provided
-if [ "$#" -lt 2 ]; then
-    echo "Usage: $0 <DATASET> <MODEL1> [MODEL2] [MODEL3] ..."
+# Check if at least one dataset and one model are provided
+if [ "$#" -lt 3 ]; then
+    echo "Usage: $0 <DATASET1> [DATASET2 ...] -- <MODEL1> [MODEL2 ...]"
     exit 1
 fi
 
-# Extract dataset from CLI arguments
-dataset="$1"
-shift  # Remove first argument (dataset) from the list
+# Separate datasets and models using '--' as a delimiter
+datasets=()
+models=()
+delimiter_found=0
 
-# Remaining arguments are the models
-models=("$@")
-
-# Loop over each model and run Snakemake
-for model in "${models[@]}"; do
-    echo "Running Snakemake for dataset: $dataset and model: $model"
-
-    snakemake --snakefile cami2_benchmark/Snakefile \
-              --config DATASET=$dataset MODEL=$model CHECKM2=True \
-              --use-conda
-
-    # Check if Snakemake was successful
-    if [ $? -ne 0 ]; then
-        echo "Snakemake failed for model: $model in dataset: $dataset"
-        exit 1
+for arg in "$@"; do
+    if [ "$arg" == "--" ]; then
+        delimiter_found=1
+        continue
     fi
+
+    if [ "$delimiter_found" -eq 0 ]; then
+        datasets+=("$arg")
+    else
+        models+=("$arg")
+    fi
+done
+
+
+# Loop through each dataset and model
+for dataset in "${datasets[@]}"; do
+    for model in "${models[@]}"; do
+        echo "Running Snakemake for dataset: $dataset and model: $model"
+
+        snakemake --snakefile cami2_benchmark/Snakefile \
+                  --config DATASET=$dataset MODEL=$model CHECKM2=True \
+                  --use-conda
+
+        if [ $? -ne 0 ]; then
+            echo "Snakemake failed for model: $model in dataset: $dataset"
+            exit 1
+        fi
+    done
 done
