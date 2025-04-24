7c2ea8ff8276c512455536ddca4c78453b3fb47e
ah140797 <ah140797@gmail.com>
2025-04-24 11:03:38 +0200

    ds

--- cami2_benchmark/src/parse_all_results.py
+++ cami2_benchmark/src/parse_all_results.py
@@ -392,9 +392,9 @@ if __name__ == "__main__":
 
     # knn_histograms = parse_knn_histograms(MODEL_RESULTS_DIR)
 
-    contig_summary, contig_lengths = parse_contig_lengths(PROCESSED_DATA_DIR)
+    # contig_summary, contig_lengths = parse_contig_lengths(PROCESSED_DATA_DIR)
 
-    runtimes = parse_runtimes(BASE_DIR)
+    # runtimes = parse_runtimes(BASE_DIR)
 
     bin_postprocess = parse_bin_postprocess(LOG_DIR)
 
