b52665ba4e7ab2ffdf8009acd5cae5ad051d83ca
ah140797 <ah140797@gmail.com>
2025-04-29 08:49:29 +0200

    ds

--- phenotype_mil/slurm_t2dew_binning.sh
+++ phenotype_mil/slurm_t2dew_binning.sh
@@ -49,7 +49,8 @@ echo "Running on node: $(hostname)"; nvidia-smi
 #Run the Snakemake pipeline
 
 snakemake --snakefile ~/l40_test/DNA-language-models-and-GLP1-microbiome-changes/phenotype_mil/Snakefile --config DATASET=T2D-EW MODEL=dnaberts CHECKM2=True --use-conda --cores all \
---rerun-incomplete --unlock
+--rerun-incomplete 
+#--unlock
 
 
 echo "Job completed successfully."
