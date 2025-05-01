91baf07af768cd03a73a49cef5f05590cd526b13
ah140797 <ah140797@gmail.com>
2025-05-01 16:00:25 +0200

    ds

--- phenotype_mil/src/utils.py
+++ phenotype_mil/src/utils.py
@@ -102,7 +102,7 @@ def read_sample_labels(
     if not split_train_test:
         assert len(sample_ids) == len(labels)
         log.append(f"Sample Ids: {sample_ids} \n Labels: {labels}")
-        return np.array(sample_ids), np.array(labels)
+        return sample_ids, np.array(labels)
 
     sample_ids_train, sample_ids_test, labels_train, labels_test = (
         split_samples_traintest(sample_ids, labels, proportion=0.3)
