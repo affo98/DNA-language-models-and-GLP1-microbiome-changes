c089160f914bc3a2f14f8e23ed5f8f4578ff2cba
ah140797 <ah140797@gmail.com>
2025-05-06 09:15:51 +0200

    ds

--- binning/test_fp16_allo_slurm.sh
+++ binning/test_fp16_allo_slurm.sh
@@ -9,9 +9,9 @@
 #SBATCH --nodes=1
 #SBATCH --exclude=cn[3-18]
 #SBATCH --exclude=desktop[1-16]
-#SBATCH --time=7-00:00:00
+#SBATCH --time=01:00:00
 #SBATCH --nodelist=cn19
-#SBATCH --gres=gpu:l40s:4
+#SBATCH --gres=gpu:l40s:1
 
 # #SBATCH --partition=scavenge
 # #SBATCH --time=04:00:00
