12e27e30723a88a9a019bb5cd8a4d6ccd087a685
ah140797 <ah140797@gmail.com>
2025-05-06 12:48:10 +0200

    ds

--- binning/src/threshold.py
+++ binning/src/threshold.py
@@ -98,7 +98,7 @@ class Threshold:
 
             centroid_similarities = torch.bmm(top_k_embeddings, centroids).squeeze(-1)
             centroid_similarities_flat = centroid_similarities.flatten()
-            print(centroid_similarities)
+            print(top_k_embeddings)
 
             global_min = torch.min(global_min, centroid_similarities_flat.min())
             global_max = torch.max(global_max, centroid_similarities_flat.max())
