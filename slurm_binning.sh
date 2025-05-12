5830e6189261df27be50d6bc609242fabaca4038
ah140797 <ah140797@gmail.com>
2025-05-12 12:04:28 +0200

    ds

--- slurm_binning.sh
+++ slurm_binning.sh
@@ -92,7 +92,7 @@ echo "Running on node: $(hostname)"; nvidia-smi
 SNAKEFILE=~/l40_test/DNA-language-models-and-GLP1-microbiome-changes/phenotype_mil/Snakefile
 WORKDIR=~/l40_test/DNA-language-models-and-GLP1-microbiome-changes
 CONFIG="--config DATASET=T2D-EW MODEL=dnaberts CHECKM2=True"
-snakemake --snakefile "$SNAKEFILE" --directory "$WORKDIR" $CONFIG --use-conda --cores all --rerun-incomplete --rerun-triggers mtime #add --unlock here if necessary
+snakemake --snakefile "$SNAKEFILE" --directory "$WORKDIR" $CONFIG --use-conda --cores all --rerun-incomplete --rerun-triggers mtime --unlock #add --unlock here if necessary
 
 
 
