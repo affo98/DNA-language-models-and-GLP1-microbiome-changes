7259b838806dd4d4a45823c2b2e9775a7bfdfcdd
ah140797 <ah140797@gmail.com>
2025-05-14 15:25:14 +0200

    ds

--- phenotype_mil/phenotype_mil.py
+++ phenotype_mil/phenotype_mil.py
@@ -51,8 +51,8 @@ SCORING_LOGISTIC = "roc_auc"
 # params sparse group lasso logistic
 # GROUP_REGS = np.logspace(-4, 4, 10)
 # L1_REGS = np.logspace(-4, 4, 10)
-GROUP_REGS = [0.00000001, 0.01, 0.1, 1, 10, 100, 1000]
-L1_REGS = [0.00000001, 0.01, 0.1, 1, 10, 100, 1000]
+GROUP_REGS = [0.0001, 0.001, 0.01, 0.1, 1, 10, 100, 1000]
+L1_REGS = [0.0001, 0.001, 0.01, 0.1, 1, 10, 100, 1000]
 
 
 def main(args, log):
