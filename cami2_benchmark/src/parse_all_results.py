8e99074ffae9e0fe2e9fe49314b51d00a41110b4
ah140797 <ah140797@gmail.com>
2025-05-19 13:18:12 +0200

    ds

--- cami2_benchmark/src/parse_all_results.py
+++ cami2_benchmark/src/parse_all_results.py
@@ -372,6 +372,7 @@ def parse_heatmaps(model_results_dir: str) -> pd.DataFrame:
                 "checkm2_validation_results",
                 "heatmap_data.json",
             )
+            print(filepath)
             with open(filepath, "r") as f:
                 data = json.load(f)
                 results.append(
@@ -389,18 +390,19 @@ def parse_heatmaps(model_results_dir: str) -> pd.DataFrame:
 
 
 if __name__ == "__main__":
-    os.makedirs(OUTPUT_DIR, exist_ok=True)
+    # os.makedirs(OUTPUT_DIR, exist_ok=True)
 
-    cami2_results = process_all_reports(MODEL_RESULTS_DIR)
+    # cami2_results = process_all_reports(MODEL_RESULTS_DIR)
 
-    knn_histograms = parse_knn_histograms(MODEL_RESULTS_DIR)
+    # knn_histograms = parse_knn_histograms(MODEL_RESULTS_DIR)
 
-    runtimes = parse_runtimes(BASE_DIR)
+    # runtimes = parse_runtimes(BASE_DIR)
 
-    bin_postprocess = parse_bin_postprocess(LOG_DIR)
+    # bin_postprocess = parse_bin_postprocess(LOG_DIR)
 
-    n_valtest = parse_nvaltest(MODEL_RESULTS_DIR)
+    # n_valtest = parse_nvaltest(MODEL_RESULTS_DIR)
 
-    heatmaps = parse_heatmaps(MODEL_RESULTS_DIR)
+    # heatmaps = parse_heatmaps(MODEL_RESULTS_DIR)
+    heatmaps = parse_heatmaps("./bl")
 
-    contig_summary, contig_lengths = parse_contig_lengths(PROCESSED_DATA_DIR)
+    # contig_summary, contig_lengths = parse_contig_lengths(PROCESSED_DATA_DIR)
